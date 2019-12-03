a37897bdf07e52832ac3140142bd9732006b70d1
revenage <romanvolobuev93@gmail.com>
2019-12-03 22:17:32 +0200

    up ino

--- device/NightLighter/NightLighter.ino
+++ device/NightLighter/NightLighter.ino
@@ -53,6 +53,192 @@ unsigned long lastConnectTry = 0;
 /** Current WLAN status */
 unsigned int status = WL_IDLE_STATUS;
 
+/** Is this an IP? */
+boolean isIp(String str) {
+  for (size_t i = 0; i < str.length(); i++) {
+    int c = str.charAt(i);
+    if (c != '.' && (c < '0' || c > '9')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/** IP to String? */
+String toStringIp(IPAddress ip) {
+  String res = "";
+  for (int i = 0; i < 3; i++) {
+    res += String((ip >> (8 * i)) & 0xFF) + ".";
+  }
+  res += String(((ip >> 8 * 3)) & 0xFF);
+  return res;
+}
+
+/** Load WLAN credentials from EEPROM */
+void loadCredentials()
+{
+    EEPROM.begin(512);
+    EEPROM.get(0, ssid);
+    EEPROM.get(0 + sizeof(ssid), password);
+    char ok[2 + 1];
+    EEPROM.get(0 + sizeof(ssid) + sizeof(password), ok);
+    EEPROM.end();
+    if (String(ok) != String("OK"))
+    {
+        ssid[0] = 0;
+        password[0] = 0;
+    }
+    Serial.println("Recovered credentials:");
+    Serial.println(ssid);
+    Serial.println(strlen(password) > 0 ? "********" : "<no password>");
+}
+
+/** Store WLAN credentials to EEPROM */
+void saveCredentials()
+{
+    EEPROM.begin(512);
+    EEPROM.put(0, ssid);
+    EEPROM.put(0 + sizeof(ssid), password);
+    char ok[2 + 1] = "OK";
+    EEPROM.put(0 + sizeof(ssid) + sizeof(password), ok);
+    EEPROM.commit();
+    EEPROM.end();
+}
+
+
+/** Handle root or redirect to captive portal */
+void handleRoot()
+{
+    if (captivePortal())
+    { // If caprive portal redirect instead of displaying the page.
+        return;
+    }
+    server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    server.sendHeader("Pragma", "no-cache");
+    server.sendHeader("Expires", "-1");
+
+    String Page;
+    Page += F(
+        "<html><head></head><body>"
+        "<h1>HELLO WORLD!!</h1>");
+    if (server.client().localIP() == apIP)
+    {
+        Page += String(F("<p>You are connected through the soft AP: ")) + softAP_ssid + F("</p>");
+    }
+    else
+    {
+        Page += String(F("<p>You are connected through the wifi network: ")) + ssid + F("</p>");
+    }
+    Page += F(
+        "<p>You may want to <a href='/wifi'>config the wifi connection</a>.</p>"
+        "</body></html>");
+
+    server.send(200, "text/html", Page);
+}
+
+/** Redirect to captive portal if we got a request for another domain. Return true in that case so the page handler do not try to handle the request again. */
+boolean captivePortal()
+{
+    if (!isIp(server.hostHeader()) && server.hostHeader() != (String(myHostname) + ".local"))
+    {
+        Serial.println("Request redirected to captive portal");
+        server.sendHeader("Location", String("http://") + toStringIp(server.client().localIP()), true);
+        server.send(302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+        server.client().stop();             // Stop is needed because we sent no content length
+        return true;
+    }
+    return false;
+}
+
+/** Wifi config page handler */
+void handleWifi()
+{
+    server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    server.sendHeader("Pragma", "no-cache");
+    server.sendHeader("Expires", "-1");
+
+    String Page;
+    Page += F(
+        "<html><head></head><body>"
+        "<h1>Wifi config</h1>");
+    if (server.client().localIP() == apIP)
+    {
+        Page += String(F("<p>You are connected through the soft AP: ")) + softAP_ssid + F("</p>");
+    }
+    else
+    {
+        Page += String(F("<p>You are connected through the wifi network: ")) + ssid + F("</p>");
+    }
+    Serial.println("scan start");
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    
+    Page += F("\r\n<br /><form method='POST' action='wifisave'><h4>Connect to network:</h4>");
+    if (n > 0)
+    {
+        Page += F("\r\n<p><select name=\"n\"><option disabled>Choose your WIFI</option>");
+        for (int i = 0; i < n; i++)
+        {
+            Page += String(F("\r\n<option value=")) + WiFi.SSID(i) + F(" selected>") + WiFi.SSID(i) + F("</option>");
+        }
+        Page += F("</select></p>");
+    }
+    else
+    {
+        Page += F("<p>No WIFI found</p>");
+    }
+    Page += F(
+        "<br /><input type='password' placeholder='password' name='p'/>"
+        "<br /><input type='submit' value='Connect/Disconnect'/></form>"
+        "<p>You may want to <a href='/'>return to the home page</a>.</p>"
+        "</body></html>");
+    server.send(200, "text/html", Page);
+    server.client().stop(); // Stop is needed because we sent no content length
+}
+
+/** Handle the WLAN save form and redirect to WLAN config page again */
+void handleWifiSave()
+{
+    Serial.println("wifi save");
+    server.arg("n").toCharArray(ssid, sizeof(ssid) - 1);
+    server.arg("p").toCharArray(password, sizeof(password) - 1);
+    server.sendHeader("Location", "wifi", true);
+    server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    server.sendHeader("Pragma", "no-cache");
+    server.sendHeader("Expires", "-1");
+    server.send(302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+    server.client().stop();             // Stop is needed because we sent no content length
+    saveCredentials();
+    connect = strlen(ssid) > 0; // Request WLAN connect with new credentials if there is a SSID
+}
+
+void handleNotFound()
+{
+    if (captivePortal())
+    { // If caprive portal redirect instead of displaying the error page.
+        return;
+    }
+    String message = F("File Not Found\n\n");
+    message += F("URI: ");
+    message += server.uri();
+    message += F("\nMethod: ");
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += F("\nArguments: ");
+    message += server.args();
+    message += F("\n");
+
+    for (uint8_t i = 0; i < server.args(); i++)
+    {
+        message += String(F(" ")) + server.argName(i) + F(": ") + server.arg(i) + F("\n");
+    }
+    server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    server.sendHeader("Pragma", "no-cache");
+    server.sendHeader("Expires", "-1");
+    server.send(404, "text/plain", message);
+}
+
+
+
 void setup()
 {
     delay(1000);
